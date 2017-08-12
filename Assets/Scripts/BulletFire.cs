f947663705d91619f051c388fb3b0da96974c6d7
岡実来 <MIRAI@MacBook-Pro-3.local>
2017-08-12 16:48:06 +0900

    ブラックボックスを作って、ファイアーボールを作った。

---
+++ Assets/Scripts/BulletFire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour {
+	private Vector3 hitPoint;
+	[SerializeField] FireEffectController fireEffectController;
+
+	void OnCollisionEnter()
+	{
+		Destroy (this.gameObject);
+		hitPoint = this.transform.position;
+		fireEffectController.createFire (hitPoint);
+	}
+}
---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour {
+
+	[SerializeField] GameObject bullet;
+	[SerializeField] Camera playerCamera;
+	[SerializeField] float speed = 20;
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject bullet2 = (GameObject) Instantiate (bullet, playerCamera.transform.position, Quaternion.identity);
+			Ray rayOrigin = playerCamera.ScreenPointToRay (Input.mousePosition);
+			Vector3 direction = rayOrigin.direction;
+
+			bullet2.GetComponent<Rigidbody> ().velocity = direction * speed;
+		}
+
+
+	}
+}
