de3f586b06801cb034bb792414f1572b16df79ff
岡実来 <MIRAI@MacBook-Pro-3.local>
2017-08-14 18:24:22 +0900

    花火弾に火を投げると花火が打ち上がるようにした。

--- Assets/Scripts/BulletFire.cs
+++ Assets/Scripts/BulletFire.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BulletFire : MonoBehaviour {
 	private Vector3 hitPoint;
 	[SerializeField] FireEffectController fireEffectController;
+	[SerializeField] GameObject seed;
 
 	void OnCollisionEnter(Collision other)
 	{
@@ -13,6 +14,9 @@ public class BulletFire : MonoBehaviour {
 		if (other.gameObject.tag == "Fire") {
 			other.transform.Find("fire1_add").gameObject.transform.localScale += new Vector3 (1, 1, 1);
 			Destroy (this.gameObject);
+		}  else if(other.gameObject.tag == "Seed") {
+			GameObject firework = Instantiate (seed, other.transform.position, Quaternion.Euler(-90, 0, 0));
+			Destroy (other.gameObject);
 		} else {
 			Destroy (this.gameObject);
 			hitPoint = this.transform.position;
